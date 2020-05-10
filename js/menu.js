66fcf17eabbabd11a257ffee4e4e0b41c4180cbf
Nicolas Gihoul <ngihoul@hotmail.com>
2020-05-10 09:50:03 +0200

    09-05-2020
    Mobile version finished

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,21 @@
 var menuHam = document.getElementById("menuHam");
 var menuMain = document.getElementById("menuMain");
-var mainNavOpen = document.getElementById("mainNavOpen");
-var footerNavOpen = document.getElementById("footerNavOpen");
+// var mainNavOpen = document.getElementById("mainNavOpen");
+// var footerNavOpen = document.getElementById("footerNavOpen");
 
 if(menuHam != null && menuMain != null && mainNavOpen != null && footerNavOpen != null) {
 	menuHam.addEventListener('click', function(e){
 		if(menuMain.className == 'l-header-nav') {
 			menuMain.className = 'l-header-nav is-active';
 			menuHam.className = 'l-header-hamburger nav-active';
-			mainNavOpen.className = 'l-main l-header-nav-active';
-			footerNavOpen.className = 'l-footer l-header-nav-active';
+			// mainNavOpen.className = 'l-main l-header-nav-active';
+			// footerNavOpen.className = 'l-footer l-header-nav-active';
 		}
 		else {
 			menuMain.className = 'l-header-nav';
 			menuHam.className = 'l-header-hamburger';
-			mainNavOpen.className = 'l-main';
-			footerNavOpen.className = 'l-footer';
+			// mainNavOpen.className = 'l-main';
+			// footerNavOpen.className = 'l-footer';
 		}
 	});
 }
