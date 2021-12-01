73cee174811957edb58a0f9011ef852e9047e830
Nicolas Gihoul <60482237+ngihoul@users.noreply.github.com>
2021-12-01 18:03:14 +0100

    Last version

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,9 @@
-var menuHam = document.getElementById("menuHam");
-var menuMain = document.getElementById("menuMain");
-// var mainNavOpen = document.getElementById("mainNavOpen");
-// var footerNavOpen = document.getElementById("footerNavOpen");
+var menuHam = document.getElementById("menuHam"),
+	menuMain = document.getElementById("menuMain");
 
-if(menuHam != null && menuMain != null && mainNavOpen != null && footerNavOpen != null) {
+if(menuHam != null && menuMain!= null) {
 	menuHam.addEventListener('click', function(e){
-		if(menuMain.className == 'l-header-nav') {
-			menuMain.className = 'l-header-nav is-active';
-			menuHam.className = 'l-header-hamburger nav-active';
-			// mainNavOpen.className = 'l-main l-header-nav-active';
-			// footerNavOpen.className = 'l-footer l-header-nav-active';
-		}
-		else {
-			menuMain.className = 'l-header-nav';
-			menuHam.className = 'l-header-hamburger';
-			// mainNavOpen.className = 'l-main';
-			// footerNavOpen.className = 'l-footer';
-		}
+		menuMain.classList.toggle("is-active");
+		menuHam.classList.toggle("nav-active");
 	});
 }
